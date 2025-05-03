8332a652852281203808167d8525a7397fa3879a
Varungowda.K <114556086+Varungowda21@users.noreply.github.com>
2025-05-03 21:55:55 +0530

    Update authUser.js

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -2,17 +2,20 @@ import jwt from 'jsonwebtoken';
 
 const authUser = async (req, res, next)=>{
     const {token} = req.cookies;
+    console.log(token)
 
     if(!token){
-        return res.json({ success: false, message: 'Not Authorized' });
+        return res.json({ success: false, message: 'Not Authorized no token' });
     }
 
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(tokenDecode)
         if(tokenDecode.id){
             req.body.userId = tokenDecode.id;
+            console.log(tokenDecode.id)
         }else{
-            return res.json({ success: false, message: 'Not Authorized' });
+            return res.json({ success: false, message: 'Not Authorized no user Id' });
         }
         next();
 
